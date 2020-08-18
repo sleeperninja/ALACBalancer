60170270558e3f2dfcb0ff121e04c4bd94067cfc
sleeperninja <mikeawatson6@gmail.com>
2020-08-18 12:06:44 -0400

    added support for .opus .wav .mp3 .flac input

--- ALACBalancer.ps1
+++ ALACBalancer.ps1
@@ -1,185 +1,193 @@
 ﻿<# Start Functions #>
-function Process-ALACDirectory($folderPath,$targetDecibels="-15",$threshold="1",$logPath="$($folderPath)\ALACBalancer.log",[switch]$unluckyMode){
-    if($env:ffmpeg){
+function ConvertTo-ALACDirectory($folderPath, $targetDecibels = "-16", $threshold = ".25", $maxPeak = "-.25", $logPath = "$($folderPath)\ALACBalancer.log", [switch]$unluckyMode) {
+    if ($env:ffmpeg) {
         Write-host -ForegroundColor Green "FFmpeg loaded."
-    } elseif (Test-Path "C:\program files\ffmpeg\bin\ffmpeg.exe") {
+    }
+    elseif (Test-Path "C:\program files\ffmpeg\bin\ffmpeg.exe") {
         #set path to ffmpeg here
         $env:ffmpeg = "C:\program files\ffmpeg\bin\ffmpeg.exe"
         Write-Host -ForegroundColor Yellow "FFmpeg found in program files. Env set. FFmpeg loaded."
-    } elseif (Test-Path "C:\program files (x86)\ffmpeg\bin\ffmpeg.exe"){
+    }
+    elseif (Test-Path "C:\program files (x86)\ffmpeg\bin\ffmpeg.exe") {
         $env:ffmpeg = "C:\program files (x86)\ffmpeg\bin\ffmpeg.exe"
         Write-Host -ForegroundColor Yellow "FFmpeg found in program files. Env set. FFmpeg loaded."
-    } else {
+    }
+    else {
         Write-Host -ForegroundColor Yellow "This script can not run without FFmpeg. Please install FFmpeg to C:\Program Files\FFmpeg or set your custom path to environment variable %ffmpeg%."
         Start-Sleep -Seconds 20
         exit
     }
 
-    if($unluckyMode){
-        ALAC-LogMessage -logPath $logPath -message 'I''m feeling unlucky.'
+
+    if ($unluckyMode) {
+        New-ALACLog -logPath $logPath -message 'I''m feeling unlucky.'
     }
-    if(Test-Path -LiteralPath $folderPath){
-        $files = Get-Childitem $folderPath -Recurse | Select * | Where-Object {$_.extension -eq ".m4a"} | Sort-Object -Descending
-        foreach($file in $files){
-            if($unluckyMode){
-                Process-ALACFile -filePath $file.fullname -targetDecibels $targetDecibels -threshold $threshold -logPath $logPath
-            } else {
-                Process-ALACFile -filePath $file.fullname -targetDecibels $targetDecibels -threshold $threshold -logPath $logPath -luckyMode
+    if (Test-Path -LiteralPath $folderPath) {
+        $files = Get-Childitem $folderPath -Recurse | Select-Object * | Where-Object { $_.extension -eq ".m4a", ".opus", ".wav", ".mp3", ".flac" } | Sort-Object -Descending
+        foreach ($file in $files) {
+            if ($unluckyMode) {
+                ConvertTo-LeveledALAC -filePath $file.fullname -targetDecibels $targetDecibels -threshold $threshold -maxPeak $maxPeak -logPath $logPath -unluckyMode
+            }
+            else {
+                ConvertTo-LeveledALAC -filePath $file.fullname -targetDecibels $targetDecibels -threshold $threshold -maxPeak $maxPeak -logPath $logPath
             }
         }
-    } else { 
-        ALAC-LogMessage -logPath $logPath -message '## break in Process-ALACDirectory: $folderPath isn''t a path?'
-        ALAC-LogMessage -logPath $logPath -message '#### $folderPath contents:'
-        ALAC-LogMessage -logPath $logPath -message $folderPath
+    }
+    else { 
+        New-ALACLog -logPath $logPath -message '## break in ConvertTo-ALACDirectory: $folderPath isn''t a path?'
+        New-ALACLog -logPath $logPath -message '#### $folderPath contents:'
+        New-ALACLog -logPath $logPath -message $folderPath
         break; 
     }
 }
 
-function Process-ALACFile($filePath,$targetDecibels="-15",$threshold="1",$logPath="$($filePath)`.log",[switch]$luckyMode){
-    ALAC-LogMessage -logPath $logPath -message "Processing file ""$($filePath)""."
-    if(Test-Path -LiteralPath $filePath){
-        $Name = $filePath | Split-Path -Leaf
+function ConvertTo-LeveledALAC($filePath, $targetDecibels = "-16", $threshold = ".25", $maxPeak = "-.25", $logPath = "$($filePath)`.log", [switch]$unluckyMode) {
+    New-ALACLog -logPath $logPath -message "Processing file ""$($filePath)""."
+    if (Test-Path -LiteralPath $filePath) {
         $Path = $filePath | Split-Path -Parent
-        $baseName = [io.path]::GetFileNameWithoutExtension($origFile)
         $dataFile = "$Path\LoudnessData.log"
         <# Feature: Volume Validation and DR Info
         $rangeLog = "$Path\DR_log.txt"
         $rangeLock = $false
         #>
-    } else { 
-        ALAC-LogMessage -logPath $logPath -message '## Break in Process-ALACFile: $filePath seems to be jacked'
-        ALAC-LogMessage -logPath $logPath -message '#### $filePath contents:'
-        ALAC-LogMessage -logPath $logPath -message $filePath
+    }
+    else { 
+        New-ALACLog -logPath $logPath -message '## Break in ConvertTo-LeveledALAC: $filePath seems to be jacked'
+        New-ALACLog -logPath $logPath -message '#### $filePath contents:'
+        New-ALACLog -logPath $logPath -message $filePath
         break; 
     }
-    ALAC-LogMessage -logPath $logPath -message "Reading loudness data."
+    New-ALACLog -logPath $logPath -message "Reading loudness data."
     $data = Get-LoudnessData -filePath $filePath -dataFile $dataFile -logPath $logPath
-    ALAC-LogMessage -logPath $logPath -message "Average Volume: $($data.input_i)dB. True Peak: $($data.input_tp)dB."
-    if($data){
-        ALAC-LogMessage -logPath $logPath -message "Calculating volume adjustment."
-        ALAC-LogMessage -logPath $logPath -message "Target volume: $($targetDecibels)dB (plus or minus $($threshold)dB). True Peak limited to -1dB."
+    New-ALACLog -logPath $logPath -message "Average Volume: $($data.input_i)dB. True Peak: $($data.input_tp)dB."
+    if ($data) {
+        New-ALACLog -logPath $logPath -message "Calculating volume adjustment."
+        New-ALACLog -logPath $logPath -message "Target volume: $($targetDecibels)dB (plus or minus $($threshold)dB). True Peak limited to $($maxPeak)`dB."
         $volAdjust = $targetDecibels - $($data.input_i)
-        ALAC-LogMessage -logPath $logPath -message "Adjustment: $($targetDecibels)dB - $($data.input_i)dB = $($volAdjust)dB. Seeing how much space is on the other side of True Peak."
-        if(($volAdjust -gt 0) -and ($volAdjust + $data.input_tp -gt -1)){
-            $oldVolAdjust = $volAdjust
-            $volAdjust = -1 - $data.input_tp
-            ALAC-LogMessage -logPath $logPath -message "True Peak is high (good dynamic range). Adjustment reduced to $($volAdjust)dB."
-        } else { 
-            ALAC-LogMessage -logPath $logPath -message 'Plenty of space on the other side of that peak...trust me. Rabbit is good, Rabbit is wise.'
+        New-ALACLog -logPath $logPath -message "Adjustment: $($targetDecibels)dB - $($data.input_i)dB = $($volAdjust)dB. Seeing if there's $($maxPeak)`dB for the True Peak."
+        if (($volAdjust -gt 0) -and ($volAdjust + $data.input_tp -gt $maxPeak)) {
+            $volAdjust = $maxPeak - $data.input_tp
+            New-ALACLog -logPath $logPath -message "True Peak is high (good dynamic range). Adjustment reduced to $($volAdjust)dB."
         }
-        if([math]::abs($volAdjust) -gt [math]::abs($threshold)){
-            if($luckyMode){
-                ALAC-LogMessage -logPath $logPath -message "Feeling lucky, punk? Adjusting volume of ""$($filePath)"" by $($volAdjust)dB to target average of $($targetDecibels)`dB without leaving a backup."
-                New-BalancedALAC -filePath $filePath -volAdjust $volAdjust -logPath $logPath -dataFile $dataFile -luckyMode
-                $data.input_i = $data.input_i + $volAdjust
-                $data.input_tp = $data.input_tp + $volAdjust
-            } else {
-                ALAC-LogMessage -logPath $logPath -message "Adjusting volume of ""$($filePath)"" by $($volAdjust)dB to target average of $($targetDecibels)`dB, and leaving a backup."
-                New-BalancedALAC -filePath $filePath -volAdjust $volAdjust -logPath $logPath -dataFile $dataFile
+        else { 
+            New-ALACLog -logPath $logPath -message 'Plenty of space on the other side of that peak...trust me. Rabbit is good, Rabbit is wise.'
+        }
+        if ([math]::abs($volAdjust) -gt [math]::abs($threshold)) {
+            if ($unluckyMode) {
+                New-ALACLog -logPath $logPath -message "Feeling lucky, punk? Adjusting volume of ""$($filePath)"" by $($volAdjust)dB to target average of $($targetDecibels)`dB without leaving a backup."
+                New-LeveledALAC -filePath $filePath -volAdjust $volAdjust -logPath $logPath -dataFile $dataFile
                 $data.input_i = $data.input_i + $volAdjust
                 $data.input_tp = $data.input_tp + $volAdjust
             }
-        } else {
-            ALAC-LogMessage -logPath $logPath -message "$($filePath) is already within the threshold. Cleaning up and moving on!"
-            if($logPath -eq "$filePath`.log"){
+            else {
+                New-ALACLog -logPath $logPath -message "Adjusting volume of ""$($filePath)"" by $($volAdjust)dB to target average of $($targetDecibels)`dB, and leaving a backup."
+                New-LeveledALAC -filePath $filePath -volAdjust $volAdjust -logPath $logPath -dataFile $dataFile -unluckyMode
+                $data.input_i = $data.input_i + $volAdjust
+                $data.input_tp = $data.input_tp + $volAdjust
+            }
+        }
+        else {
+            New-ALACLog -logPath $logPath -message "$($filePath) is already within the threshold. Cleaning up and moving on!"
+            if ($logPath -eq "$filePath`.log") {
                 Write-Host -ForegroundColor Green "$($filePath) is already within the threshold. Cleaning up!"
                 Remove-Item "$filePath`.log" -Force -Confirm:$false
                 Remove-Item $dataFile -Force -Confirm:$false
-            } else { 
+            }
+            else { 
                 Remove-Item $dataFile -Force -Confirm:$false
             }
         }
-    } else { 
-        ALAC-LogMessage -logPath $logPath -message '## Break in Process-ALACFile: $data missing'
-        ALAC-LogMessage -logPath $logPath -message '#### $data contents:'
-        ALAC-LogMessage -logPath $logPath -message $data
+    }
+    else { 
+        New-ALACLog -logPath $logPath -message '## Break in ConvertTo-LeveledALAC: $data missing'
+        New-ALACLog -logPath $logPath -message '#### $data contents:'
+        New-ALACLog -logPath $logPath -message $data
         break; 
     }
 }
 
-function Get-LoudnessData($filePath,$dataFile="$($filePath)`.data.log",$logPath="$($filePath)`.log"){
+function Get-LoudnessData($filePath, $dataFile = "$($filePath)`.data.log", $logPath = "$($filePath)`.log") {
     #if($filePath -like '*`[*'){ 'true' }
-    if(Test-Path -LiteralPath $filePath){
+    if (Test-Path -LiteralPath $filePath) {
         $arg1 = ' -i "{0}" -af loudnorm=I=-15:TP=-1.0:LRA=1:print_format=json -f null -' -f $filePath
         Start-Process -FilePath $env:ffmpeg -ArgumentList $arg1 -RedirectStandardError $dataFile -NoNewWindow -Wait
-    } else { 
-        ALAC-LogMessage -logPath $logPath -message '## Break in Get-LoudnessData: What is $filePath?'
-        ALAC-LogMessage -logPath $logPath -message '#### $filePath contents:'
-        ALAC-LogMessage -logPath $logPath -message $filePath
+    }
+    else { 
+        New-ALACLog -logPath $logPath -message '## Break in Get-LoudnessData: What is $filePath?'
+        New-ALACLog -logPath $logPath -message '#### $filePath contents:'
+        New-ALACLog -logPath $logPath -message $filePath
         break; 
     }
-    $data = Filter-LoudnessData -dataFile $dataFile
+    $data = Compare-LoudnessData -dataFile $dataFile
     return $data
 }
 
-function Filter-LoudnessData($dataFile,$logPath="$($dataFile)`.log"){
-    if(Test-Path -LiteralPath $dataFile){
-        $data = @{}
-        $data.raw = Get-Content -Path $dataFile -Tail 12
-        $data.input_i = $data.raw[-11].split('"')[3]
-        $data.input_tp = $data.raw[-10].split('"')[3]
-        $data.input_lra = $data.raw[-9].split('"')[3]
-        $data.input_thresh = $data.raw[-8].split('"')[3]
-        $data.output_i = $data.raw[-7].split('"')[3]
-        $data.output_tp = $data.raw[-6].split('"')[3]
-        $data.output_lra = $data.raw[-5].split('"')[3]
-        $data.output_thresh = $data.raw[-4].split('"')[3]
-        $data.normalization_type = $data.raw[-3].split('"')[3]
-        $data.target_offset = $data.raw[-2].split('"')[3]
-        $data.DR = $data.input_tp - $data.input_i
-    } else { 
-        ALAC-LogMessage -logPath $logPath -message '## Break in Filter-LoudnessData: $dataFile doesn''t exist.'
-        ALAC-LogMessage -logPath $logPath -message '#### $dataFile contents:'
-        ALAC-LogMessage -logPath $logPath -message $dataFile
+function Compare-LoudnessData($dataFile, $logPath = "$($dataFile)`.log") {
+    if (Test-Path -LiteralPath $dataFile) {
+        $rawData = Get-Content -Path $dataFile -Tail 12;
+        $data = @{
+            input_i            = $rawData[-11].split('"')[3];
+            input_tp           = $rawData[-10].split('"')[3];
+            input_lra          = $rawData[-9].split('"')[3];
+            input_thresh       = $rawData[-8].split('"')[3];
+            output_i           = $rawData[-7].split('"')[3];
+            output_tp          = $rawData[-6].split('"')[3];
+            output_lra         = $rawData[-5].split('"')[3];
+            output_thresh      = $rawData[-4].split('"')[3];
+            normalization_type = $rawData[-3].split('"')[3];
+            target_offset      = $rawData[-2].split('"')[3];
+            DR                 = $data.input_tp - $data.input_i
+        }
+    }
+    else { 
+        New-ALACLog -logPath $logPath -message '## Break in Compare-LoudnessData: $dataFile doesn''t exist.'
+        New-ALACLog -logPath $logPath -message '#### $dataFile contents:'
+        New-ALACLog -logPath $logPath -message $dataFile
         break; 
     }
     Return $data
 }
 
-function New-BalancedALAC($filePath,$volAdjust,$dataFile,$logPath="$($filePath)`.log",[switch]$luckyMode){
-    if(!($filePath)){
-        ALAC-LogMessage -logPath $logPath -message '## Break in New-BalancedALAC: $filePath is jacked.'
-        ALAC-LogMessage -logPath $logPath -message '#### $filePath contents:'
-        ALAC-LogMessage -logPath $logPath -message $filePath
+function New-LeveledALAC($filePath, $volAdjust, $dataFile, $logPath = "$($filePath)`.log", [switch]$unluckyMode) {
+    if (!($filePath)) {
+        New-ALACLog -logPath $logPath -message '## Break in New-LeveledALAC: $filePath is jacked.'
+        New-ALACLog -logPath $logPath -message '#### $filePath contents:'
+        New-ALACLog -logPath $logPath -message $filePath
         break;
     }
-    if(!($volAdjust)){
-        ALAC-LogMessage -logPath $logPath -message '## Break in New-BalancedALAC: $volAdjust is jacked.'
-        ALAC-LogMessage -logPath $logPath -message '#### $volAdjust contents:'
-        ALAC-LogMessage -logPath $logPath -message $volAdjust
+    if (!($volAdjust)) {
+        New-ALACLog -logPath $logPath -message '## Break in New-LeveledALAC: $volAdjust is jacked.'
+        New-ALACLog -logPath $logPath -message '#### $volAdjust contents:'
+        New-ALACLog -logPath $logPath -message $volAdjust
         break;
     }
-    if(!($dataFile)){
-        ALAC-LogMessage -logPath $logPath -message '## Break in New-BalancedALAC: $dataFile is jacked.'
-        ALAC-LogMessage -logPath $logPath -message '#### $dataFile contents:'
-        ALAC-LogMessage -logPath $logPath -message $dataFile
+    if (!($dataFile)) {
+        New-ALACLog -logPath $logPath -message '## Break in New-LeveledALAC: $dataFile is jacked.'
+        New-ALACLog -logPath $logPath -message '#### $dataFile contents:'
+        New-ALACLog -logPath $logPath -message $dataFile
         break;
     }
     $tempPath = "$($filePath | Split-Path -Parent)\temp_$($filePath | split-path -leaf)"
-    ALAC-LogMessage -logPath $logPath -message "Creating temp copy of ""$filePath"""
+    $newPath = "$($filePath | Split-Path -Parent)\$([io.path]::GetFileNameWithoutExtension($filePath))`.m4a"
+    New-ALACLog -logPath $logPath -message "Creating temp copy of ""$filePath"""
     Move-Item -LiteralPath $filePath -Destination $tempPath -Force
-    $arg2 = ' -i "{0}" -filter:a "volume={1}dB" -c:v copy -map_metadata:s:a 0:s:a -acodec alac -ar 44100 -sample_fmt s16p "{2}"' -f $tempPath,$volAdjust,$filePath
+    $arg2 = ' -i "{0}" -filter:a "volume={1}dB" -c:v copy -map_metadata:s:a 0:s:a -acodec alac -ar 44100 -sample_fmt s16p "{2}"' -f $tempPath, $volAdjust, $newPath
     Start-Process -FilePath $env:ffmpeg -ArgumentList $arg2 -RedirectStandardError $dataFile -NoNewWindow -Wait
-    if($luckyMode){
-        ALAC-LogMessage -logPath $logPath -message '$luckyMode enabled: deleting temp file.'
+    if ($unluckyMode) {
+        Remove-Item -LiteralPath $logPath -Confirm:$false
         Remove-Item -LiteralPath $tempPath -Confirm:$false
         Remove-Item -LiteralPath $dataFile -Confirm:$false
-    }
-    if(!($luckyMode)){
-        if((Get-ItemProperty $filePath).length -gt 0){
-        ALAC-LogMessage -logPath $logPath -message "Good news. ""$filepath"" isn't null. Feel free to delete ""$tempPath""."
-        Remove-Item -LiteralPath $dataFile -Confirm:$false
+    } 
+    else {
+        if ((Get-ItemProperty $newPath).length -gt 0) {
+            New-ALACLog -logPath $logPath -message "Good news. ""$newPath"" isn't null. Feel free to delete ""$tempPath""."
+            Remove-Item -LiteralPath $dataFile -Confirm:$false
         }
     }
 }
 
-function ALAC-LogMessage($logPath,$message){
+function New-ALACLog($logPath, $message) {
     '' | Out-File -LiteralPath $logPath -Encoding ascii -Append 
     $message | Out-File -LiteralPath $logPath -Encoding ascii -Append
 }
 <# End Functions #>
-
-
-<# Testing Space 
-Process-ALACDirectory "M:\CDFlac\Junip\Junip - Junip - 2013 (320 kbps)" -luckyMode
-#>
--- README.md
+++ README.md
@@ -1,25 +1,41 @@
 # ALACBalancer
-Uses FFmpeg to read dynamic range data from ALAC files, and adjust volume to -15dB with a -1dB true peak limit -- no normalizing.
 
-Before You Run:
+*The ALACBalancer cmdlets use FFmpeg to read dynamic range data from individual audio files (`ConvertTo-LeveledALAC`), or scans folders for audio files (`ConvertTo-ALACDirectory`), adjusting volume to -16dB (or any volume you specify), without normalizing.*
+
+
+
+This script utilizes FFmpeg's built in [EBU R 128 Loudness Normalization](https://en.wikipedia.org/wiki/EBU_R_128) loudness measuring standard to detect volume average. Once the loudness data has been attained, it compares it to the arguments provided to verify that the loudness data fall within the parameters provided. Should an audio file fall outside the parameters, it will adjust the volume to match and create a new ALAC file.
+
+## Before You Run
+
 FFmpeg.exe optimally should exist at "C:\program files\ffmpeg\bin\ffmpeg.exe" or "C:\program files (x86)\ffmpeg\bin\ffmpeg.exe". The script will set the location found as the environment variable $env:ffmpeg, which is used by the script to execute ffmpeg processes. FFmpeg can be installed in any location, as long as $env:ffmpeg contains that path.
 
-Usage:
+## Usage
+
 Load script by running .\ALACBalancer.ps1 from within the same directory in Powershell.
 
+## To process a single audio file
+
 Run
->Process-AlacDirectory -folderPath <Path to ALAC files>
->Process-AlacDirectory <Path to ALAC files>
+>ConvertTo-LeveledALAC -filePath <Path to audio file>
 
-This will automatically find all ALAC files recursively based on this location and adjust volume to average -15dB, allowing peak to be no louder than -1dB. Note that PowerShell hates files with [ or ] in the name, and is likely to fail should any paths include these characters. 
+Run
+>ConvertTo-ALACDirectory -folderPath <Path to audio files>
 
-Other script arguments:
--folderPath <Path to ALAC files> 
--targetDecibels: the default is "-15", but any number can be set here to determine the average volume.
--threshold: the default is "1", which actually means -1dB. You can set this to "0" if you prefer to let the peaks touch the waveform maximum.
--logPath: the default is "$($folderPath)\ALACBalancer.log". This means that it extracts the specified folder path above and maintains the process log.
--unluckyMode: this will preserve the original copy of your ALAC file if you're afraid you're about to jack them up.
+This will search for audio files recursively based on the specified path, adjust volume to average -16dB, allowing peak to be no louder than -.25dB. 
 
-Note:
-This script is only searching for files with the extension .m4a when processing. If you run this on a location with AAC files, they will be processed and converted to ALAC (say goodbye to space savings), with no actual increase in quality.
+## Other script arguments
+
+- folderPath is the only required argument
+- targetDecibels: the default is "-16", but any number can be set here to determine the average volume. 
+- threshold: this is the swing range in average DR around which the script will ignore the file and move on.
+- maxPeak: the default is "-.25", or -.25dB. This prevents the waveform from peaking, as the DA process can produce a waveform that's still capable of clipping, and therefore capable of damaging audio equipment.
+- logPath: the default is "$($folderPath)\ALACBalancer.log". This means that it extracts the specified folder path above and maintains the process log.
+- unluckyMode: this will preserve the original copy of your ALAC file if you're afraid you're about to jack them up. I trust the script, but I don't expect you to trust it until you've seen it at work.
+
+## Current Issues
+
+- PowerShell hates square brackets (`[` or `]`). While I'm currently working on speed improvements in the script, the current state will potentially choke on any path or file with square brackets. If a file or path only includes one square bracket, it's guarenteed to fail.
+- Currently supported input file types: .m4a (doesn't know the difference between aac and alac), .opus, .wav, .mp3, .flac
+- May not transition metadata from filetypes other than AAC or ALAC
 
